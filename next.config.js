e4c0bdd0170be7c7426fee8674ec13156d6dbe7c
SebastianAvila <jesusaviala@gmail.com>
2026-01-09 17:01:41 -0600

    fixedv1

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: ['open.cruip.com','ucarecdn.com', 'www.svgrepo.com','images.unsplash.com', 'res.cloudinary.com']
-    },
-    webpack(config) {
-        config
-            .module
-            .rules
-            .push({test: /\.svg$/, use: ["@svgr/webpack"]});
+  reactStrictMode: true,
 
-        return config;
-    }
-}
+  // 🔥 DESACTIVAR minificación problemática
+  swcMinify: false,
 
-module.exports = nextConfig
+  images: {
+    domains: [
+      'open.cruip.com',
+      'ucarecdn.com',
+      'www.svgrepo.com',
+      'images.unsplash.com',
+      'res.cloudinary.com',
+    ],
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
